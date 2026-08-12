70722cb01f547699c48dc5523cf1d381ac6d826d
AugustoBerto <z3augustoberto@gmail.com>
2026-08-12 16:54:50 -0300

    fix: vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: env.VITE_PUBLIC_BASE || "/",
+    base: "./",
     plugins: [vue()],
     resolve: {
       alias: {
